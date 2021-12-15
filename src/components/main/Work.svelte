f9a38b0dae6b92987b47cde9b94e5ece9002a7e6
Adekunbi Adefolarin <dekunbifola100@gmail.com>
2021-12-15 10:57:24 +0100

    feat: added habita356

--- src/components/main/Work.svelte
+++ src/components/main/Work.svelte
@@ -13,6 +13,48 @@
     </div>
     <div class="mb-20">
       <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div class="col-span-1">
+          <div class="flex h-full items-center">
+            <div>
+              <p class="mb-4">Featured Project</p>
+              <h3 class="mb-8 text-3xl font-bold">Habita365</h3>
+              <div
+                class="p-6 mb-4 bg-green text-white relative rounded m-minus-r-4"
+              >
+                Habita365 is a platform where people can buy, sell and rent homes.
+              </div>
+              <div class="mb-4">
+                <span class="mr-2">Expressjs</span>
+                <span class="mr-2">Tailwindcss</span>
+                <span class="mr-2">Reactjs</span>
+                <span class="mr-2">MongoDB</span>
+                <span class="mr-2">Nextjs</span>
+              </div>
+              <div>
+                <a
+                  href="https://www.habita365.com.br"
+                  class="block"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/images/external.svg" alt="Open in another tab" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-span-1 lg:col-span-2">
+          <div>
+            <picture class="w-full h-full">
+              <source srcset="/images/1.avif" type="image/avif" />
+              <img src="/images/4.jpg" alt="Arquibusco" />
+            </picture>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mb-20">
+      <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
         <div class="col-span-1 lg:col-span-2">
           <div>
             <picture class="w-full h-full">
@@ -48,48 +90,6 @@
                 </a>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="mb-20">
-      <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        <div class="col-span-1">
-          <div class="flex h-full items-center">
-            <div>
-              <p class="mb-4">Featured Project</p>
-              <h3 class="mb-8 text-3xl font-bold">Arquibusco</h3>
-              <div
-                class="p-6 mb-4 bg-green text-white relative rounded m-minus-r-4"
-              >
-                Arquibusco is a platform that connect Freelancer to employers.
-              </div>
-              <div class="mb-4">
-                <span class="mr-2">Expressjs</span>
-                <span class="mr-2">Tailwindcss</span>
-                <span class="mr-2">Reactjs</span>
-                <span class="mr-2">MongoDB</span>
-                <span class="mr-2">Nextjs</span>
-              </div>
-              <div>
-                <a
-                  href="https://arquibusco.com"
-                  class="block"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src="/images/external.svg" alt="Open in another tab" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-span-1 lg:col-span-2">
-          <div>
-            <picture class="w-full h-full">
-              <source srcset="/images/1.avif" type="image/avif" />
-              <img src="/images/1.jpg" alt="Arquibusco" />
-            </picture>
           </div>
         </div>
       </div>
